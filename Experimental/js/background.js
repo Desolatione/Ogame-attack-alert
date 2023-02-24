d680368685caf5c4abeaa2689140814a2c38e2c2
Desolatione <124031581+Desolatione@users.noreply.github.com>
2023-02-24 16:06:52 +0100

    Add files via upload

---
+++ Experimental/js/background.js
@@ -0,0 +1,36 @@
+// Listen for messages from content scripts
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.type === "notification") {
+        // Create desktop notification
+        chrome.notifications.create({
+            type: "basic",
+            title: "Ogame Attack Notifier",
+            message: "You are under attack!",
+            iconUrl: "128.png"
+        });
+    }
+});
+
+// Set up popup
+chrome.action.onClicked.addListener((tab) => {
+    chrome.action.setPopup({ tabId: tab.id, popup: "popup.html" });
+});
+
+// Listen for when the extension is installed or updated
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason === "install") {
+        // Open options page after installation
+        chrome.runtime.openOptionsPage();
+    } else if (details.reason === "update") {
+        var thisVersion = chrome.runtime.getManifest().version;
+        console.log("Updated from version " + details.previousVersion + " to " + thisVersion + ".");
+    }
+});
+
+// Set up periodic check for new attacks
+setInterval(function() {
+    // Send message to content script to check for new attacks
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: "check_attacks"});
+    });
+}, 60000);
